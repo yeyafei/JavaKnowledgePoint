ca6a4d24c196d538d3e46b9d9b5119a0b9ea23c2
yeyafei <yeyafei@hotmail.com>
2018-12-05 17:31:33 +0800

    注释

--- spi-demo/spi-main/src/main/java/com/yyf/SpiTest.java
+++ spi-demo/spi-main/src/main/java/com/yyf/SpiTest.java
@@ -4,6 +4,7 @@ import java.util.ServiceLoader;
 
 public class SpiTest {
     public static void main(String[] args) {
+        //修改POM动态调用implement-one/implement-two
         ServiceLoader<Print> printerLoader = ServiceLoader.load(Print.class);
         for (Print print : printerLoader) {
             print.doSomething();
